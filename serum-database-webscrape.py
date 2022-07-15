24d5f9039f62464959095353a43bab2040304d5d
Pufferfish-School <jdganson@iu.edu>
2022-07-15 07:32:28 -0400

    finished scraping first page

--- serum-database-webscrape.py
+++ serum-database-webscrape.py
@@ -21,7 +21,6 @@ def get_links(parsed):
 		links.append(link)
 	return links
 
-#this does not separate out abundance by which compound it is.  most compounds have many abundances.  still need to construct a list within 'abundances' per molecule
 def find_abundances(links):
 	abundances = []
 	for i in range(len(links)):
@@ -49,16 +48,16 @@ def get_structure_link(parsed):
 		link = "https://serummetabolome.ca" + link
 		links.append(link)
 	return links
-
+	
+# uncomment to get images saved
 def get_structures(parsed, names):
 	links = get_structure_link(parsed)
-	# print(links)
 	structures_filenames = []
 	for i in range(len(links)):
 		webpage = requests.get(links[i])
 		filename = names[i]
-		with open(filename, 'wb') as f:
-			f.write(webpage.content)
+		# with open(filename + ".svg", 'wb') as f:
+		# 	f.write(webpage.content)
 		structures_filenames.append(filename)
 	return structures_filenames
 
@@ -71,7 +70,6 @@ def find_weights(links):
 		parsed_webpage = parsed_webpage.find_all("th")
 		text = "Monoisotopic Molecular Weight"
 		for tags in parsed_webpage:
-			print(tags)
 			if tags.string == text:
 				parsed_webpage = tags.find_next_siblings("td")
 				for whatever in parsed_webpage:
@@ -114,9 +112,12 @@ def main():
 	# data_table = make_data_table(names, links, structures_filenames, weights, abundances)
 	print("names:", len(names))
 	for i in range(len(names)):
-		print(names[i], ":", boilingpoint[i])
-		print(names[i], ":", meltingpoint[i])
-		print(names[i], ":", solubility[i])
+		print(names[i])
+		print("abundance:", abundances[i])
+		print("mw:", weights[i])
+		print("meltingpoint:", meltingpoint[i])
+		print("boilingpoint:", boilingpoint[i])
+		print("solubility:", solubility[i])
 
 
 if __name__ == '__main__':
